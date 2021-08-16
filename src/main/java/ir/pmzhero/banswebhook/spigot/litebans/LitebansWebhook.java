7784c42701b66bedf6d5b9ee6e6ba5b37d95bc29
PmzHeroV2 <81774395+PmzHeroV2@users.noreply.github.com>
2021-08-16 05:41:22 -0700

    Fixed Spigot Version Bugs

--- src/main/java/ir/pmzhero/banswebhook/spigot/litebans/LitebansWebhook.java
+++ src/main/java/ir/pmzhero/banswebhook/spigot/litebans/LitebansWebhook.java
@@ -4,7 +4,6 @@ package ir.pmzhero.banswebhook.spigot.litebans;
 import club.minnced.discord.webhook.WebhookClient;
 import club.minnced.discord.webhook.send.WebhookEmbed;
 import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
-import ir.pmzhero.banswebhook.utils.ConfigManager;
 import litebans.api.Database;
 import litebans.api.Entry;
 import litebans.api.Events;
@@ -25,8 +24,8 @@ public class LitebansWebhook {
             @Override
             public void entryAdded(Entry entry) {
                 if (entry.getType().equals(entryType)) {
-                    if (entry.isSilent() && ConfigManager.getConfig().getBoolean("litebans-do-not-send-silent-punishments")) return;
-                    if (!ConfigManager.getConfig().getSection(entryType).getBoolean("enabled")) return;
+                    if (entry.isSilent() && instance.getConfig().getBoolean("litebans-do-not-send-silent-punishments")) return;
+                    if (!instance.getConfig().getConfigurationSection(entryType).getBoolean("enabled")) return;
                     String uuid = entry.getUuid();
                     String query = "SELECT name FROM {history} WHERE uuid=? ORDER BY date DESC LIMIT 1";
                     try (PreparedStatement st = Database.get().prepareStatement(query)) {
@@ -34,15 +33,15 @@ public class LitebansWebhook {
                         try (ResultSet rs = st.executeQuery()) {
                             if (rs.next()) {
                                 String playerName = rs.getString("name");
-                                WebhookClient client = WebhookClient.withUrl(ConfigManager.getConfig().getSection(entryType).getString("webhook-url"));
-                                WebhookEmbed.EmbedTitle title = new WebhookEmbed.EmbedTitle(ConfigManager.getConfig().getSection(entryType).getString("webhook-title"), "");
+                                WebhookClient client = WebhookClient.withUrl(instance.getConfig().getConfigurationSection(entryType).getString("webhook-url"));
+                                WebhookEmbed.EmbedTitle title = new WebhookEmbed.EmbedTitle(instance.getConfig().getConfigurationSection(entryType).getString("webhook-title"), "");
                                 WebhookEmbed.EmbedField field1 = new WebhookEmbed.EmbedField(false, punishedBy, Objects.requireNonNull(entry.getExecutorName()));
                                 WebhookEmbed.EmbedField field2 = new WebhookEmbed.EmbedField(false, "Username", playerName);
                                 WebhookEmbed.EmbedField field3 = new WebhookEmbed.EmbedField(false, "Reason", Objects.requireNonNull(entry.getReason()));
                                 WebhookEmbed.EmbedField field4 = new WebhookEmbed.EmbedField(false, "Duration", Objects.requireNonNull(entry.getDurationString()));
                                 WebhookEmbed embed = new WebhookEmbedBuilder()
                                         .setTitle(title)
-                                        .setThumbnailUrl(ConfigManager.getConfig().getSection(entryType).getString("webhook-thumbnail"))
+                                        .setThumbnailUrl(instance.getConfig().getConfigurationSection(entryType).getString("webhook-thumbnail"))
                                         .setColor(0xFF0000)
                                         .addField(field1)
                                         .addField(field2)
@@ -52,19 +51,7 @@ public class LitebansWebhook {
                                 client.send(embed);
 
 
-                                    /*
 
-                                    webhook.addEmbed(new DiscordWebhook.EmbedObject()
-                                            .setTitle(ConfigManager.getConfig().getSection(entryType).getString("webhook-title"))
-                                            .setThumbnail(ConfigManager.getConfig().getSection(entryType).getString("webhook-thumbnail"))
-                                            .setColor(Color.RED)
-                                            .addField(punishedBy, entry.getExecutorName(), false)
-                                            .addField("Username:", playerName, false)
-                                            .addField("Reason:", entry.getReason(), false)
-                                            .addField("Duration:", entry.getDurationString(), false));
-                                    webhook.execute();
-
-                                     */
                             }
                         }
                     } catch (SQLException e) {
